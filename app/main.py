d9aa0eacc1ebe787bf75fb8ad82c7cc72ba805c1
Aratrika03Das <dasaratrika03@gmail.com>
2025-08-19 16:02:25 +0530

    Changed 3 main.py

--- app/main.py
+++ app/main.py
@@ -12,6 +12,7 @@ app = FastAPI(title="Email Spam-Ham Classifier API")
 origins = [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
+    "https://mlops-rg.azurewebsites.net",
     "https://mlops-rg-hcfvaqdkgmdfd8a9.centralindia-01.azurewebsites.net/"
 ]
 
